faba330f479b6a829b23f4803d81de00c07cb438
Gor Petrosyan <gpetrosyan.ggm@gmail.com>
2023-05-10 02:57:51 +0400

    fixed leave command factory creating

--- src/main/java/gp/assessments/chat/common/factory/CommandFactory.java
+++ src/main/java/gp/assessments/chat/common/factory/CommandFactory.java
@@ -19,7 +19,7 @@ public class CommandFactory {
                 command.init(params);
                 yield new CommandMapperModel(command, new JoinCommandHandler());
             }
-            case LEAVE_COMMAND -> new CommandMapperModel(new LeaveCommand(), new JoinCommandHandler());
+            case LEAVE_COMMAND -> new CommandMapperModel(new LeaveCommand(), new LeaveCommandHandler());
             case DISCONNECT_COMMAND -> new CommandMapperModel(new DisconnectCommand(), new DisconnectCommandHandler());
             case CHANNELS_COMMAND -> new CommandMapperModel(new ChannelsCommand(), new ChannelsCommandHandler());
             case USERS_COMMAND -> new CommandMapperModel(new UsersCommand(), new UsersCommandHandler());
