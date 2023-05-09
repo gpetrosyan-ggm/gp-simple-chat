e96676b73b66d3bdad4d4174c078fa1238f0c231
Gor Petrosyan <gpetrosyan.ggm@gmail.com>
2023-05-09 23:52:32 +0400

    implemented auth logic

---
+++ src/main/java/gp/assessments/chat/auth/AuthService.java
@@ -0,0 +1,7 @@
+package gp.assessments.chat.auth;
+
+public interface AuthService {
+
+    void authenticate(String user, String password);
+
+}
--- src/main/java/gp/assessments/chat/common/entity/UserEntity.java
+++ src/main/java/gp/assessments/chat/common/entity/UserEntity.java
@@ -1,13 +1,27 @@
 package gp.assessments.chat.common.entity;
 
 import lombok.Getter;
-import lombok.Setter;
+
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @Getter
-@Setter
 public class UserEntity {
 
-    private String id;
-    private String name;
+    private final String id;
+    private final String userName;
+    private final String password;
+    private final AtomicBoolean online;
+
+    public UserEntity(String userName, String password) {
+        this.id = UUID.randomUUID().toString();
+        this.userName = userName;
+        this.password = password;
+        this.online = new AtomicBoolean(false);
+    }
+
+    public void makeOnline() {
+        online.compareAndSet(false, true);
+    }
 
 }
