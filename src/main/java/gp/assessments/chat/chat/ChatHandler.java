f2f491b5cd28b6ba6ccdc57504dbd45598a68b76
Gor Petrosyan <gpetrosyan.ggm@gmail.com>
2023-05-11 01:28:26 +0400

    added custom exception handler

--- src/main/java/gp/assessments/chat/chat/ChatHandler.java
+++ src/main/java/gp/assessments/chat/chat/ChatHandler.java
@@ -1,12 +1,20 @@
 package gp.assessments.chat.chat;
 
+import gp.assessments.chat.ChatApplication;
+import gp.assessments.chat.common.error.SimpleChatBaseException;
 import gp.assessments.chat.common.model.CommandMapperModel;
+import gp.assessments.chat.utils.PropertiesUtils;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Objects;
 
 @RequiredArgsConstructor
 public class ChatHandler extends SimpleChannelInboundHandler<CommandMapperModel> {
+    private static final Logger logger = LoggerFactory.getLogger(ChatHandler.class);
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, CommandMapperModel model) throws Exception {
@@ -15,7 +23,17 @@ public class ChatHandler extends SimpleChannelInboundHandler<CommandMapperModel>
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-        ctx.writeAndFlush(String.format("Error: %s\r\n", cause.getMessage()));
+        SimpleChatBaseException customException = (SimpleChatBaseException) cause.getCause();
+        if (Objects.isNull(customException)) {
+            customException = (SimpleChatBaseException) cause;
+        }
+        logger.error("Error type: {}, Error code: {}, Error message: {}",
+                     customException.getErrorType(),
+                     customException.getErrorType().getErrorCode(),
+                     customException.getMessage());
+        ctx.writeAndFlush(String.format("%s%s",
+                                        PropertiesUtils.getAsString(customException.getErrorType().getErrorCodeProp()),
+                                        "\r\n"));
     }
 
 }
